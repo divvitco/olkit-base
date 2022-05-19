fb1ab2576827cff47091d8c99502515d01df62f6
Noah Tatko <tatkon@rbtrainers.com>
2022-05-19 12:44:28 -0500

    hobble demo together

--- demo/typescript/src/App.tsx
+++ demo/typescript/src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react"
+import OlMap from "ol/Map"
 
-const { MapProvider, Map } = require('olkit-core')
+const { MapProvider, Map, loadDataLayer } = require('@olkit/core')
 
 function App() {
+  const onMapInit = (map: OlMap): void => {
+    window.map = map // nice to have for debugging purposes
+
+    const layer = loadDataLayer(map, "https://data.nasa.gov/api/geospatial/7zbq-j77a?method=export&format=KML")
+  }
+
   return (
     <div className="App">
-      <MapProvider>
+      <MapProvider onMapInit={onMapInit}>
         <Map />
       </MapProvider>
     </div>
